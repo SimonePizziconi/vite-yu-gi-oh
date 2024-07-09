8d8baa357745fa1f55a4ce6698cca5429f5fbdbd
SimonePizziconi <simonepizziconi05@gmail.com>
2024-07-09 16:20:55 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+
+export default{
+  name: "app",
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<template>
+  <AppHeader/>
+</template>
+
+<style>
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,33 @@
+<script>
+    export default{
+        name: "AppHeader"
+    }
+</script>
+
+<template>
+    <div class="header-container">
+        <div class="logo-container">   
+            <img src="https://upload.wikimedia.org/wikipedia/commons/1/11/Yu-Gi-Oh%21_%28Logo%29.jpg" alt="Logo Yu-Gi-Oh!">
+        </div>
+        <h1>Yu-Gi-Oh Api</h1>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+    .header-container{
+        display: flex;
+        height: 100px;
+
+        .logo-container{
+        width: 200px;
+        display: flex;
+
+        img{
+            width: 100%;
+            background-color: #000;
+        }
+    }
+    }
+    
+</style>
