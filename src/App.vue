5d463adca7acabffaddd315c026008ea4ba64811
SimonePizziconi <simonepizziconi05@gmail.com>
2024-07-09 16:42:02 +0200

    feat: create "AppCardContainer" component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <script>
 import AppHeader from "./components/AppHeader.vue";
+import AppCardContainer from "./components/AppCardContainer.vue"
 
 export default{
   name: "app",
   components: {
-    AppHeader
+    AppHeader,
+    AppCardContainer
   }
 }
 </script>
 
 <template>
   <AppHeader/>
+  <main>
+    <AppCardContainer/>
+  </main>
 </template>
 
-<style>
+<style lang="scss">
+@use "../src/style/partials/variables.scss" as *;
+@use "../src/style/partials/mixins.scss" as *;
+
+  main{
+    background-color: $bg-color;
+    @include centerFlex();
+
+  }
+  
+  
+
 </style>
---
+++ src/components/AppCardContainer.vue
@@ -0,0 +1,33 @@
+<script>
+    export default{
+        name: "AppCardContainer"
+    }
+</script>
+
+<template>
+    <div class="card-container">
+        <div class="cards-found">   
+            <h3>Sono State Trovate 20 Carte</h3>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+    .card-container{
+        width: 70%;
+        margin: 0 auto;
+        background-color: #fff;
+        padding: 50px;
+        margin-top: 100px;
+        // DEBUG
+        height: 1000px;
+
+        .cards-found{
+            background-color: #212529;
+            color: #fff;
+            padding: 20px;
+        }
+    }
+
+</style>
